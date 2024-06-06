29cbfd70f4853baab137c2ff94898e3b119f85d1
HENRIQUE DE OLIVEIRA Pedro <pedro2.oliveira@egis-group.com>
2024-06-06 20:25:09 -0300

    teste

--- 1Crop_intire_2_.py
+++ 1Crop_intire_2_.py
@@ -29,7 +29,7 @@ def pdf_to_full_png(pdf_path, output_dir, zoom_factor=5.0):
 
     print(f"Todas as páginas do arquivo {pdf_name} foram processadas e salvas.")
 
-def process_all_pdfs_in_directory(input_dir, output_dir, zoom_factor=5.0):
+def process_all_pdfs_in_directory(input_dir, output_dir, zoom_factor=2.0):
     # Verificar se o diretório de saída existe, se não, criar
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
